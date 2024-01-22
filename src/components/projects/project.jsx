69fe13e1dbfb288e08e83f7d0964f0c22148f7fe
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-01-22 16:24:05 +0600

    completed

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
@@ -6,30 +6,51 @@ import { faLink } from "@fortawesome/free-solid-svg-icons";
 import "./styles/project.css";
 
 const Project = (props) => {
-	const { logo, title, description, linkText, link } = props;
+  const { logo, title, description, linkText, link } = props;
 
-	return (
-		<React.Fragment>
-			<div className="project">
-				<Link to={link}>
-					<div className="project-container">
-						<div className="project-logo">
-							<img src={logo} alt="logo" />
-						</div>
-						<div className="project-title">{title}</div>
-						<div className="project-description">{description}</div>
-						<div className="project-link">
-							<div className="project-link-icon">
-								<FontAwesomeIcon icon={faLink} />
-							</div>
+  const maxDescriptionChars = 10;
+  const [isExpanded, setIsExpanded] = useState(false);
 
-							<div className="project-link-text">{linkText}</div>
-						</div>
-					</div>
-				</Link>
-			</div>
-		</React.Fragment>
-	);
+  const toggleAccordion = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const truncateDescription = isExpanded
+    ? description
+    : `${description.slice(0, maxDescriptionChars)}...`;
+
+  return (
+    <React.Fragment>
+      <div className="project">
+        {/* <Link to={link}> */}
+        <div className="project-container">
+          <div className="project-logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="project-title">{title}</div>
+
+          <div className="project-description" onClick={toggleAccordion}>
+            {description}
+            {truncateDescription}
+            {description.length > maxDescriptionChars && (
+              <span style={{ cursor: "pointer", color: "#14b8a6" }}>
+                {isExpanded ? " See less" : " See more"}
+              </span>
+            )}
+          </div>
+
+          <Link to={link} className="project-link">
+            <div className="project-link-icon">
+              <FontAwesomeIcon icon={faLink} />
+            </div>
+
+            <div className="project-link-text">{linkText}</div>
+          </Link>
+        </div>
+        {/* </Link> */}
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Project;
